a923a935a5a9bfb7025dccc7630f7485eefa4dca
Aidan Hobson Sayers <aidanhs@cantab.net>
2021-02-16 22:56:29 +0000

    More reduction

--- interp-py.c
+++ interp-py.c
@@ -5,12 +5,6 @@
 #define DELIM L':'
 #define MAXPATHLEN 4096
 #define SEP L'/'
-#define PYTHONPATH ":plat-linux"
-#define PREFIX "/home/aidanhs/Desktop/per/bsaber/bsmeta/plugins/cpython/dist"
-#define EXEC_PREFIX "/home/aidanhs/Desktop/per/bsaber/bsmeta/plugins/cpython/dist"
-#define VERSION "3.5"
-#define VPATH ""
-#define LANDMARK L"os.py"
 static wchar_t exec_prefix[MAXPATHLEN+1];
 
 static int
@@ -115,7 +109,7 @@ copy_absolute(wchar_t *path, wchar_t *p, size_t pathlen)
 */
 
 static int
-find_env_config_value(FILE * env_file, const wchar_t * key, wchar_t * value)
+find_env_config_value(FILE * env_file, const wchar_t * key)
 {
     int result = 0; /* meaning not found */
     char buffer[MAXPATHLEN*2+1];  /* allow extra for key, '=', etc. */
@@ -139,23 +133,11 @@ find_env_config_value(FILE * env_file, const wchar_t * key, wchar_t * value)
         decoded = PyUnicode_DecodeUTF8(buffer, n, "surrogateescape");
         if (decoded != NULL) {
             Py_ssize_t k;
-            wchar_t * state;
             k = PyUnicode_AsWideChar(decoded,
                                      tmpbuffer, MAXPATHLEN * 2);
             Py_DECREF(decoded);
             if (k >= 0) {
-                wchar_t * tok = wcstok(tmpbuffer, L" \t\r\n", &state);
-                if ((tok != NULL) && !wcscmp(tok, key)) {
-                    tok = wcstok(NULL, L" \t", &state);
-                    if ((tok != NULL) && !wcscmp(tok, L"=")) {
-                        tok = wcstok(NULL, L"\r\n", &state);
-                        if (tok != NULL) {
-                            wcsncpy(value, tok, MAXPATHLEN);
-                            result = 1;
-                            break;
-                        }
-                    }
-                }
+                abort();
             }
         }
     }
@@ -166,24 +148,10 @@ find_env_config_value(FILE * env_file, const wchar_t * key, wchar_t * value)
    MAXPATHLEN bytes long.
 */
 static int
-search_for_exec_prefix(wchar_t *argv0_path, wchar_t *home,
+search_for_exec_prefix(wchar_t *argv0_path,
                        wchar_t *_exec_prefix, wchar_t *lib_python)
 {
     size_t n;
-
-    /* If PYTHONHOME is set, we believe it unconditionally */
-    if (home) {
-        wchar_t *delim;
-        delim = wcschr(home, DELIM);
-        if (delim)
-            wcsncpy(exec_prefix, delim+1, MAXPATHLEN);
-        else
-            wcsncpy(exec_prefix, home, MAXPATHLEN);
-        exec_prefix[MAXPATHLEN] = L'\0';
-        joinpath(exec_prefix, lib_python);
-        joinpath(exec_prefix, L"lib-dynload");
-        return 1;
-    }
 
     /* Check to see if argv[0] is in the build directory. "pybuilddir.txt"
        is written by setup.py and contains the relative path to the location
@@ -246,8 +214,6 @@ search_for_exec_prefix(wchar_t *argv0_path, wchar_t *home,
 static void
 _calculate_path(void)
 {
-    wchar_t *home = NULL;
-    wchar_t *path_buffer = NULL;
     wchar_t argv0_path[MAXPATHLEN+1];
     argv0_path[0] = '\0';
     wchar_t zip_path[MAXPATHLEN+1];
@@ -258,10 +224,10 @@ _calculate_path(void)
     wchar_t *lib_python;
     fprintf(stderr, "calc 1\n");
 
-    _pythonpath = Py_DecodeLocale(PYTHONPATH, NULL);
-    _prefix = Py_DecodeLocale(PREFIX, NULL);
-    _exec_prefix = Py_DecodeLocale(EXEC_PREFIX, NULL);
-    lib_python = Py_DecodeLocale("lib/python" VERSION, NULL);
+    _pythonpath = Py_DecodeLocale(":plat-linux", NULL);
+    _prefix = L"/home/aidanhs/Desktop/per/bsaber/bsmeta/plugins/cpython/dist";
+    _exec_prefix = L"/home/aidanhs/Desktop/per/bsaber/bsmeta/plugins/cpython/dist";
+    lib_python = L"lib/python3.5";
     fprintf(stderr, "calc 2\n");
 
     if (!_pythonpath || !_prefix || !_exec_prefix || !lib_python) {
@@ -285,28 +251,20 @@ _calculate_path(void)
 
     fprintf(stderr, "calc 5 =%ls= %p %lu\n", _pythonpath, _pythonpath, wcslen(_pythonpath));
     {
-        wchar_t tmpbuffer[MAXPATHLEN+1];
-        wchar_t *env_cfg = L"pyvenv.cfg";
         FILE * env_file = NULL;
 
-        wcscpy(tmpbuffer, argv0_path);
-
-        joinpath(tmpbuffer, env_cfg);
-        env_file = _Py_wfopen(tmpbuffer, L"r");
+        env_file = _Py_wfopen(L"/dev/null", L"r");
         if (env_file == NULL) {
             errno = 0;
-            reduce(tmpbuffer);
-            reduce(tmpbuffer);
-            joinpath(tmpbuffer, env_cfg);
-            env_file = _Py_wfopen(tmpbuffer, L"r");
+            env_file = NULL;
             if (env_file == NULL) {
                 errno = 0;
             }
         }
         if (env_file != NULL) {
             /* Look for a 'home' variable and set argv0_path to it, if found */
-            if (find_env_config_value(env_file, L"home", tmpbuffer)) {
-                wcscpy(argv0_path, tmpbuffer);
+            if (find_env_config_value(env_file, L"home")) {
+                abort();
             }
             fclose(env_file);
             env_file = NULL;
@@ -318,12 +276,9 @@ _calculate_path(void)
     fprintf(stderr, "zip_path %lu\n", wcslen(zip_path));
 
     fprintf(stderr, "calc 6 =%ls= %p %lu\n", _pythonpath, _pythonpath, wcslen(_pythonpath));
-    efound = search_for_exec_prefix(argv0_path, home,
-                                    _exec_prefix, lib_python);
+    efound = search_for_exec_prefix(argv0_path, _exec_prefix, lib_python);
     if (!efound) {
-        wcsncpy(exec_prefix, _exec_prefix, MAXPATHLEN);
-        fprintf(stderr, "!efound %lu %lu\n", wcslen(_exec_prefix), wcslen(exec_prefix));
-        joinpath(exec_prefix, L"lib/lib-dynload");
+        fprintf(stderr, "!efound\n");
     }
 
     bufsz = 200;
